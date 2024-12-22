537e36b7bce85507ef1efee09566141567846a0b
Akshay Mathur <akshaymathur242@gmail.com>
2024-12-23 02:37:43 +0530

    .

--- Assessment/src/components/StudentRow.jsx
+++ Assessment/src/components/StudentRow.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
-const StudentRow = ({ student }) => {
+const StudentRow = ({ student, isFirstRow }) => {
   return (
-    <tr>
-      <td className="px-4 py-2">{student.name}</td>
-      <td className="px-4 py-2">{student.cohort}</td>
-      <td className="px-4 py-2">{student.courses.join(", ")}</td>
-      <td className="px-4 py-2">{student.date_joined}</td>
-      <td className="px-4 py-2">{student.last_login}</td>
-      <td className="px-4 py-2">
-        <span
-          className={`inline-block w-3 h-3 rounded-full ${
-            student.status === "active" ? "bg-green-500" : "bg-red-500"
-          }`}
-        ></span>
-      </td>
-    </tr>
+    <>
+      {!isFirstRow && (
+        <tr>
+          <td colSpan="6">
+            <hr className="border-gray-300 my-0" />
+          </td>
+        </tr>
+      )}
+      <tr>
+        <td className="px-4 py-2 text-sm">{student.name}</td>
+        <td className="px-4 py-2 text-sm">{student.cohort}</td>
+        <td className="px-6 py-2 text-sm">{student.courses.join(", ")}</td>
+        <td className="px-5 py-2 text-sm">{student.date_joined}</td>
+        <td className="px-5 py-2 text-sm">{student.last_login}</td>
+        <td className="px-8 py-2 text-sm">
+          <span
+            className={`inline-block w-2.5 h-2.5 rounded-full ${
+              student.status === "active" ? "bg-green-500" : "bg-red-500"
+            }`}
+          ></span>
+        </td>
+      </tr>
+    </>
   );
 };
 
--- Assessment/src/components/StudentTable.jsx
+++ Assessment/src/components/StudentTable.jsx
@@ -8,10 +8,10 @@ const StudentTable = ({ students }) => {
         <tr>
           <th className="px-4 py-2 text-left">Student Name</th>
           <th className="px-4 py-2 text-left">Cohort</th>
-          <th className="px-4 py-2 text-left">Courses</th>
-          <th className="px-4 py-2 text-left">Date Joined</th>
-          <th className="px-4 py-2 text-left">Last Login</th>
-          <th className="px-4 py-2 text-left">Status</th>
+          <th className="px-6 py-2 text-left">Courses</th>
+          <th className="px-5 py-2 text-left">Date Joined</th>
+          <th className="px-5 py-2 text-left">Last Login</th>
+          <th className="px-4 py-2 text-left ">Status</th>
         </tr>
       </thead>
       <tbody>
